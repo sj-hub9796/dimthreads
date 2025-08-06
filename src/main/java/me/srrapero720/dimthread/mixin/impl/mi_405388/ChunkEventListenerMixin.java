71c0a850467995f253b5b21c8883153098de86df
sjhub <sj.hub9796@gmail.com>
2025-08-06 17:42:55 +0900

    i don't need this

--- src/main/java/me/srrapero720/dimthread/mixin/impl/mi_405388/ChunkEventListenerMixin.java
+++ src/main/java/me/srrapero720/dimthread/mixin/impl/mi_405388/ChunkEventListenerMixin.java
@@ -8,24 +8,24 @@ import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "aztech.modern_industrialization.machines.multiblocks.world")
-@Pseudo
-public class ChunkEventListenerMixin {
-    @Redirect(method = "onBlockStateChange", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;isSameThread()Z;"))
-    @Dynamic
-    private static boolean redirect$isSameThread(MinecraftServer instance) {
-        return true;
-    }
-
-    /**
-     * @author SrRapero720
-     * @reason Redundant
-     */
-    @Overwrite
-    private static void ensureServerThread(MinecraftServer server) {
-        if (server == null) {
-            throw new RuntimeException("Null server!");
-        }
-        // removed "isSameThread" check
-    }
-}
+//@Mixin(targets = "aztech.modern_industrialization.machines.multiblocks.world")
+//@Pseudo
+//public class ChunkEventListenerMixin {
+//    @Redirect(method = "onBlockStateChange", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;isSameThread()Z;"))
+//    @Dynamic
+//    private static boolean redirect$isSameThread(MinecraftServer instance) {
+//        return true;
+//    }
+//
+//    /**
+//     * @author SrRapero720
+//     * @reason Redundant
+//     */
+//    @Overwrite
+//    private static void ensureServerThread(MinecraftServer server) {
+//        if (server == null) {
+//            throw new RuntimeException("Null server!");
+//        }
+//        // removed "isSameThread" check
+//    }
+//}
